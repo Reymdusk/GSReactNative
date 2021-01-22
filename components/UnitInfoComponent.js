d5c7872613c447ec209d001c26d01a06909cf126
John Nguyen <aznxmist@gmail.com>
2021-01-22 16:35:47 -0600

    Cards for Skills

--- components/UnitInfoComponent.js
+++ components/UnitInfoComponent.js
@@ -1,7 +1,7 @@
 //TODO Basic Unit Detail structure
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Image } from 'react-native-elements';
+import { Image, Card } from 'react-native-elements';
 import { UNITINFO } from '../shared/unitInfo';
 
 class UnitInfo extends Component {
@@ -19,8 +19,9 @@ class UnitInfo extends Component {
 
     render(){
         return (
-            <View>
+            <View key={unitinfo.id}>
                 <RenderDetail detail={unitinfo.image.detail} />
+                <RenderStats stats={unitinfo.stats} attr={unitinfo.attribute} type={unitinfo.type} />
             </View>
         );
     }
@@ -34,7 +35,40 @@ function RenderDetail(detail) {
     );
 }
 
-function RenderStats(hp, atk, def) {
-    
+function RenderStats(stats, attr, type) {
+    return(
+        <Card>
+            <Card.Title>STATS</Card.Title> 
+            <Card.Divider />
+            <Text>{attr}, {type}</Text>
+            <Text>HP: {stats.hp} + {stats.hpplus}</Text>
+            <Text>ATK: {stats.atk} + {stats.atkplus}</Text>
+            <Text>DEF: {stats.def} + {stats.defplus}</Text>
+        </Card>
+    );
 }
 
+function RenderSkillset(skillset) {
+    return (
+        <Card>
+            <Card.Title>SKILLSET</Card.Title>
+            <Card.Divider />
+            <Text>Skill: {skillset.skill} BREAK {skillset.skillbreak}</Text>
+            <Text>Arts: {skillset.arts} BREAK {skillset.artsbreak}</Text>
+            <Text>True Arts: {skillset.truearts} BREAK {skillset.trueartsbreak}</Text>
+        </Card>
+    );
+}
+
+function RenderPassive(passive){
+    return(
+        <Card>
+            <Card.Title>PASSIVES</Card.Title>
+            <Card.Divider />
+            <Text>Ability: </Text>
+            <Text>Ability: </Text>
+        </Card>
+    )
+}
+
+export default UnitInfo;
